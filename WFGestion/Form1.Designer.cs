96c069149dc4a0dc5418ab587420eec5485f30b7
DESKTOP-F7POVF1\Virgen de Gracia <isma.cama.sanc@gmail.com>
2025-09-30 17:16:55 +0200

    Diseño de la pestaña 1: "Datos personales".

--- WFGestion/Form1.Designer.cs
+++ WFGestion/Form1.Designer.cs
@@ -36,14 +36,32 @@
             this.btGuardar = new System.Windows.Forms.Button();
             this.btSalir = new System.Windows.Forms.Button();
             this.btLimpiar = new System.Windows.Forms.Button();
+            this.lbNombre = new System.Windows.Forms.Label();
+            this.lbDireccion = new System.Windows.Forms.Label();
+            this.lbCorreo = new System.Windows.Forms.Label();
+            this.tbCorreo = new System.Windows.Forms.TextBox();
+            this.tbDireccion = new System.Windows.Forms.TextBox();
+            this.tbNombre = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lbSexo = new System.Windows.Forms.Label();
+            this.rbMasculino = new System.Windows.Forms.RadioButton();
+            this.rbFemenino = new System.Windows.Forms.RadioButton();
+            this.tbOtro = new System.Windows.Forms.RadioButton();
+            this.monthCalendar1 = new System.Windows.Forms.MonthCalendar();
+            this.lbFechaNac = new System.Windows.Forms.Label();
+            this.nudEdad = new System.Windows.Forms.NumericUpDown();
+            this.lbEdad = new System.Windows.Forms.Label();
             this.tabControl1.SuspendLayout();
+            this.tbDatos.SuspendLayout();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nudEdad)).BeginInit();
             this.SuspendLayout();
             // 
             // lbTitulo
             // 
             this.lbTitulo.AutoSize = true;
             this.lbTitulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lbTitulo.Location = new System.Drawing.Point(417, 53);
+            this.lbTitulo.Location = new System.Drawing.Point(417, 54);
             this.lbTitulo.Name = "lbTitulo";
             this.lbTitulo.Size = new System.Drawing.Size(541, 29);
             this.lbTitulo.TabIndex = 0;
@@ -54,7 +72,7 @@
             this.tabControl1.Controls.Add(this.tbDatos);
             this.tabControl1.Controls.Add(this.tpPreferencias);
             this.tabControl1.Controls.Add(this.tpVisualizacion);
-            this.tabControl1.Location = new System.Drawing.Point(12, 85);
+            this.tabControl1.Location = new System.Drawing.Point(12, 86);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
             this.tabControl1.Size = new System.Drawing.Size(1447, 573);
@@ -62,6 +80,18 @@
             // 
             // tbDatos
             // 
+            this.tbDatos.Controls.Add(this.lbEdad);
+            this.tbDatos.Controls.Add(this.nudEdad);
+            this.tbDatos.Controls.Add(this.lbFechaNac);
+            this.tbDatos.Controls.Add(this.monthCalendar1);
+            this.tbDatos.Controls.Add(this.lbSexo);
+            this.tbDatos.Controls.Add(this.panel1);
+            this.tbDatos.Controls.Add(this.tbNombre);
+            this.tbDatos.Controls.Add(this.tbDireccion);
+            this.tbDatos.Controls.Add(this.tbCorreo);
+            this.tbDatos.Controls.Add(this.lbCorreo);
+            this.tbDatos.Controls.Add(this.lbDireccion);
+            this.tbDatos.Controls.Add(this.lbNombre);
             this.tbDatos.Location = new System.Drawing.Point(4, 25);
             this.tbDatos.Name = "tbDatos";
             this.tbDatos.Padding = new System.Windows.Forms.Padding(3);
@@ -92,7 +122,7 @@
             // btGuardar
             // 
             this.btGuardar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btGuardar.Location = new System.Drawing.Point(229, 679);
+            this.btGuardar.Location = new System.Drawing.Point(229, 680);
             this.btGuardar.Name = "btGuardar";
             this.btGuardar.Size = new System.Drawing.Size(191, 70);
             this.btGuardar.TabIndex = 2;
@@ -102,7 +132,7 @@
             // btSalir
             // 
             this.btSalir.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btSalir.Location = new System.Drawing.Point(1024, 679);
+            this.btSalir.Location = new System.Drawing.Point(1024, 680);
             this.btSalir.Name = "btSalir";
             this.btSalir.Size = new System.Drawing.Size(191, 70);
             this.btSalir.TabIndex = 3;
@@ -112,12 +142,152 @@
             // btLimpiar
             // 
             this.btLimpiar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btLimpiar.Location = new System.Drawing.Point(612, 679);
+            this.btLimpiar.Location = new System.Drawing.Point(612, 680);
             this.btLimpiar.Name = "btLimpiar";
             this.btLimpiar.Size = new System.Drawing.Size(191, 70);
             this.btLimpiar.TabIndex = 4;
             this.btLimpiar.Text = "LIMPIAR";
             this.btLimpiar.UseVisualStyleBackColor = true;
+            // 
+            // lbNombre
+            // 
+            this.lbNombre.AutoSize = true;
+            this.lbNombre.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbNombre.Location = new System.Drawing.Point(69, 105);
+            this.lbNombre.Name = "lbNombre";
+            this.lbNombre.Size = new System.Drawing.Size(81, 25);
+            this.lbNombre.TabIndex = 0;
+            this.lbNombre.Text = "Nombre";
+            // 
+            // lbDireccion
+            // 
+            this.lbDireccion.AutoSize = true;
+            this.lbDireccion.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbDireccion.Location = new System.Drawing.Point(513, 105);
+            this.lbDireccion.Name = "lbDireccion";
+            this.lbDireccion.Size = new System.Drawing.Size(93, 25);
+            this.lbDireccion.TabIndex = 1;
+            this.lbDireccion.Text = "Dirección";
+            // 
+            // lbCorreo
+            // 
+            this.lbCorreo.AutoSize = true;
+            this.lbCorreo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbCorreo.Location = new System.Drawing.Point(1003, 105);
+            this.lbCorreo.Name = "lbCorreo";
+            this.lbCorreo.Size = new System.Drawing.Size(171, 25);
+            this.lbCorreo.TabIndex = 2;
+            this.lbCorreo.Text = "Correo electrónico";
+            // 
+            // tbCorreo
+            // 
+            this.tbCorreo.Location = new System.Drawing.Point(960, 161);
+            this.tbCorreo.Name = "tbCorreo";
+            this.tbCorreo.Size = new System.Drawing.Size(257, 22);
+            this.tbCorreo.TabIndex = 3;
+            // 
+            // tbDireccion
+            // 
+            this.tbDireccion.Location = new System.Drawing.Point(441, 161);
+            this.tbDireccion.Name = "tbDireccion";
+            this.tbDireccion.Size = new System.Drawing.Size(275, 22);
+            this.tbDireccion.TabIndex = 4;
+            // 
+            // tbNombre
+            // 
+            this.tbNombre.Location = new System.Drawing.Point(23, 161);
+            this.tbNombre.Name = "tbNombre";
+            this.tbNombre.Size = new System.Drawing.Size(177, 22);
+            this.tbNombre.TabIndex = 5;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.tbOtro);
+            this.panel1.Controls.Add(this.rbFemenino);
+            this.panel1.Controls.Add(this.rbMasculino);
+            this.panel1.Location = new System.Drawing.Point(40, 277);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(303, 216);
+            this.panel1.TabIndex = 6;
+            // 
+            // lbSexo
+            // 
+            this.lbSexo.AutoSize = true;
+            this.lbSexo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbSexo.Location = new System.Drawing.Point(139, 221);
+            this.lbSexo.Name = "lbSexo";
+            this.lbSexo.Size = new System.Drawing.Size(58, 25);
+            this.lbSexo.TabIndex = 7;
+            this.lbSexo.Text = "Sexo";
+            // 
+            // rbMasculino
+            // 
+            this.rbMasculino.AutoSize = true;
+            this.rbMasculino.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rbMasculino.Location = new System.Drawing.Point(26, 26);
+            this.rbMasculino.Name = "rbMasculino";
+            this.rbMasculino.Size = new System.Drawing.Size(106, 24);
+            this.rbMasculino.TabIndex = 0;
+            this.rbMasculino.TabStop = true;
+            this.rbMasculino.Text = "Masculino";
+            this.rbMasculino.UseVisualStyleBackColor = true;
+            // 
+            // rbFemenino
+            // 
+            this.rbFemenino.AutoSize = true;
+            this.rbFemenino.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rbFemenino.Location = new System.Drawing.Point(26, 94);
+            this.rbFemenino.Name = "rbFemenino";
+            this.rbFemenino.Size = new System.Drawing.Size(103, 24);
+            this.rbFemenino.TabIndex = 1;
+            this.rbFemenino.TabStop = true;
+            this.rbFemenino.Text = "Femenino";
+            this.rbFemenino.UseVisualStyleBackColor = true;
+            // 
+            // tbOtro
+            // 
+            this.tbOtro.AutoSize = true;
+            this.tbOtro.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbOtro.Location = new System.Drawing.Point(26, 166);
+            this.tbOtro.Name = "tbOtro";
+            this.tbOtro.Size = new System.Drawing.Size(63, 24);
+            this.tbOtro.TabIndex = 2;
+            this.tbOtro.TabStop = true;
+            this.tbOtro.Text = "Otro";
+            this.tbOtro.UseVisualStyleBackColor = true;
+            // 
+            // monthCalendar1
+            // 
+            this.monthCalendar1.Location = new System.Drawing.Point(579, 286);
+            this.monthCalendar1.Name = "monthCalendar1";
+            this.monthCalendar1.TabIndex = 8;
+            // 
+            // lbFechaNac
+            // 
+            this.lbFechaNac.AutoSize = true;
+            this.lbFechaNac.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbFechaNac.Location = new System.Drawing.Point(588, 240);
+            this.lbFechaNac.Name = "lbFechaNac";
+            this.lbFechaNac.Size = new System.Drawing.Size(199, 25);
+            this.lbFechaNac.TabIndex = 9;
+            this.lbFechaNac.Text = "Fecha de nacimiento:";
+            // 
+            // nudEdad
+            // 
+            this.nudEdad.Location = new System.Drawing.Point(987, 426);
+            this.nudEdad.Name = "nudEdad";
+            this.nudEdad.Size = new System.Drawing.Size(230, 22);
+            this.nudEdad.TabIndex = 10;
+            // 
+            // lbEdad
+            // 
+            this.lbEdad.AutoSize = true;
+            this.lbEdad.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbEdad.Location = new System.Drawing.Point(1003, 369);
+            this.lbEdad.Name = "lbEdad";
+            this.lbEdad.Size = new System.Drawing.Size(199, 25);
+            this.lbEdad.TabIndex = 11;
+            this.lbEdad.Text = "Fecha de nacimiento:";
             // 
             // Form1
             // 
@@ -132,6 +302,11 @@
             this.Name = "Form1";
             this.Text = "Gestión Avanzada de Información Personal.";
             this.tabControl1.ResumeLayout(false);
+            this.tbDatos.ResumeLayout(false);
+            this.tbDatos.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nudEdad)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -147,6 +322,21 @@
         private System.Windows.Forms.Button btGuardar;
         private System.Windows.Forms.Button btSalir;
         private System.Windows.Forms.Button btLimpiar;
+        private System.Windows.Forms.Label lbNombre;
+        private System.Windows.Forms.Label lbCorreo;
+        private System.Windows.Forms.Label lbDireccion;
+        private System.Windows.Forms.TextBox tbCorreo;
+        private System.Windows.Forms.TextBox tbNombre;
+        private System.Windows.Forms.TextBox tbDireccion;
+        private System.Windows.Forms.Label lbSexo;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.RadioButton tbOtro;
+        private System.Windows.Forms.RadioButton rbFemenino;
+        private System.Windows.Forms.RadioButton rbMasculino;
+        private System.Windows.Forms.Label lbFechaNac;
+        private System.Windows.Forms.MonthCalendar monthCalendar1;
+        private System.Windows.Forms.Label lbEdad;
+        private System.Windows.Forms.NumericUpDown nudEdad;
     }
 }
 
